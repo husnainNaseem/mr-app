ea2b321fcc48f8120468f4c68d0684c05f95060a
husnainNaseem <husnaintanoli6@gmail.com>
2022-02-25 00:03:26 +0500

    Fix loginwithgoogle signin

--- src/components/signin.js
+++ src/components/signin.js
@@ -45,7 +45,7 @@ const Login = (_props) => {
             email:user.email,
             phoneNumber:user.phoneNumber
         });
-        window.location.href = "/";
+        window.location.href = "/Homepage";
         updateProfile(auth.currentUser, {
           displayName: "Jane Q. User",
           photoURL: "https://example.com/jane-q-user/profile.jpg",
